3fc9d99c9b33eeff50fcd9345a4842d589699dce
HankGIMM <sidewixer@gmail.com>
2025-12-08 00:54:50 -0700

    AI state machine

---
+++ Assets/Scripts/SeagullStates/SeagullDeadState.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+/// <summary>
+/// Seagull Dead State - Final state before destruction.
+/// Cleans up and notifies spawner.
+/// </summary>
+public class SeagullDeadState : ISeagullState
+{
+    public void OnEnter(SeagullController controller)
+    {
+        // Notify spawner and destroy
+        controller.NotifyAndDestroy();
+    }
+
+    public void OnUpdate(SeagullController controller)
+    {
+        // Do nothing - just waiting for destruction
+    }
+
+    public void OnExit(SeagullController controller)
+    {
+        // Nothing needed
+    }
+}
